f10bca9efd745bc3cf5487237d8184452f4c035c
Arthur Bandeira <arthur.baandeira@gmail.com>
2024-04-29 19:11:45 -0300

    iniciando projeto

---
+++ src/main/java/com/arthurbandeira/manageparking/ManageparkingApplication.java
@@ -0,0 +1,13 @@
+package com.arthurbandeira.manageparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageparkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurbandeira/manageparking/ManageparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurbandeira.manageparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
